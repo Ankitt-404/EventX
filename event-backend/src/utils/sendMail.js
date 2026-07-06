b05572e26712a26d8bdcb226534e80e296209b57
ankitjaiswal1153-sde <ankitjaiswal1153@gmail.com>
2026-07-06 20:45:41 +0530

    resend otp fix

--- event-backend/src/utils/sendMail.js
+++ event-backend/src/utils/sendMail.js
@@ -1,14 +1,20 @@
 import resend from "./mailer.js";
 
-
-
-
 export const sendEmail = async ({ to, subject, html }) => {
   
-await resend.emails.send({
-  from: "onboarding@resend.dev",
- to,
-    subject,
-    html: html || `<p>${message}</p>`,
-});
+  try{
+    console.log("to:", to)
+    const response = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to,
+      subject,
+      html,
+    });
+    console.log("Successfull", response)
+    return response
+  }
+  catch(error){
+    console.error(error)
+    throw error
+  }
 }
